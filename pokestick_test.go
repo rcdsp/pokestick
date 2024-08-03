9620b8c6664e292b6a84a717ec596c410152ccb7
rcdsp <richard.cdsp@gmail.com>
2024-08-03 13:26:26 -0600

    Cleanup updated of .gitignore

--- pokestick_test.go
+++ pokestick_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestGurl(t *testing.T) {
+func TestPokeStick(t *testing.T) {
 	t.Run("Parse expression", func(t *testing.T) {
 		got := resolveExpression("${api_key}")
 		want := "ApiKey"
